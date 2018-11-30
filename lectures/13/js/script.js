f9407671b54cfad9f503f7babf2d567adfd3a9ab
ABUBAKER7 <abubakerch7@outlook.com>
2018-11-30 19:05:09 +0500

    Quiz4

--- lectures/13/js/script.js
+++ lectures/13/js/script.js
@@ -13,13 +13,33 @@ var questions = [{
         "None of the above"],
     correctAnswer : 2
 },{
-    question : "Using _______ statement is how you test for a specific condition",
-    choices : [ "select",
-        "if",
-        "for",
-        "none of the above"],
-    correctAnswer : 1
-}];
+        question : "founder of google ",
+        choices : [ "select",
+            "Larry Page and Sergey Brin",
+            "Larry Page",
+            "Sergey Brin",
+        "None of above"],
+        correctAnswer : 1
+    }
+    ,{
+        question : "html stands for ",
+        choices : [ "select",
+            "Hyper Text Language",
+            "Hper TExt Language",
+             "Hyper Text Markup Language",
+        "None of above"],
+        correctAnswer : 3
+    },
+    {
+        question : "Using _______ statement is how you test for a specific condition",
+        choices : [ "select",
+            "if",
+            "for",
+            "none of the above"],
+        correctAnswer : 1
+    }
+
+    ];
 
 var currentQuestion = 0;
 var correctAnswers = 0;
@@ -28,12 +48,38 @@ displayCurrentQuestion();
 document.getElementById("quiz-message").style.display = 'none';
 function displayNext() {
     /*Write your code here */
+    displayCurrentQuestion();
+    if(currentQuestion>=4){
+        displayScore();
+    }
+    if(document.getElementById("next-btn")) {
+        if(currentQuestion==5)
+        {
+            document.getElementById("result").innerText="your score is  =" + correctAnswers;
+
+            document.getElementById("quiz-message").innerText="play again  =";
+
+        }
+        currentQuestion++;
+        displayCurrentQuestion();
+    }
+
 }
 
 function displayCurrentQuestion() {
     /*Write your code here */
-}
+    document.getElementById("question").innerText = questions[currentQuestion].question;
+    document.getElementById("choice-list").innerHTML="";
+    for (var a = 0; a < 4; a++)
+    {
+        document.getElementById("choice-list").innerHTML += '<li><input type="radio" name="q">'+questions[currentQuestion].choices[a]+'</li>';
 
+    }
+    if(document.querySelector("input[name='q']:checked")==null)
+    {
+
+    }
+}
 function resetQuiz() {
     currentQuestion = 0;
     correctAnswers = 0;
