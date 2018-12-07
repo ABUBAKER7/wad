06ce5f1ce45abc8b7f13828e5ceed161247ba598
ABUBAKER7 <abubakerch7@outlook.com>
2018-12-07 19:19:12 +0500

    mob task complete

---
+++ lectures/15/js/main.js
@@ -0,0 +1,78 @@
+var info = {
+    title : "ABUBAKER",
+    balance : 500000,
+    currency : "PKR",
+    IBAN : "PKN0095837421"
+};
+
+var acc_balance = document.getElementById("balance");
+var acc_title = document.getElementById("title");
+var acc_currency = document.getElementById("currency");
+var acc_IBAN = document.getElementById("IBAN");
+var acc_deposit = document.getElementById("deposit");
+var acc_withdraw = document.getElementById("withdraw");
+
+
+display_data();
+
+function display_data() {
+
+
+    acc_title.innerText = info.title;
+    acc_balance.value = info.balance;
+    acc_balance.innerText = acc_balance.value;
+    acc_currency.innerText = info.currency;
+    acc_IBAN.innerText = info.IBAN;
+
+}
+
+function deposit()
+{
+    document.getElementById("deposit-msg").innerText = "";
+    if(event.key === "Enter")
+    {
+        var data = parseInt(acc_deposit.value,10);
+        if(data>0) {
+            acc_balance.value += data;
+            acc_balance.innerText = acc_balance.value;
+            acc_deposit.value = "";
+            var table_transaction = document.getElementById("transaction-table");
+            table_transaction.innerHTML += '<tr>' +
+                '<td>' + Date() + '</td>' +
+                '<td>' + "Credit" + '</td>' +
+                '<td>' + data + '</td>' +
+                '</tr>';
+        }
+        else{
+            var error_msg = document.getElementById("deposit-msg");
+            error_msg.innerText = "Enter valid input";
+        }
+
+    }
+}
+
+function withdraw()
+{
+    document.getElementById("withdraw-msg").innerText = "";
+    if(event.key === "Enter")
+    {
+        var data = parseInt(acc_withdraw.value,10);
+        if(data <= acc_balance.value && data>0)
+        {
+            acc_balance.value = acc_balance.value - data;
+            acc_balance.innerText = acc_balance.value;
+            acc_withdraw.value = "";
+            var table_transaction = document.getElementById("transaction-table");
+            table_transaction.innerHTML+='<tr>' +
+                '<td>'+Date()+'</td>' +
+                '<td align="left" valign="middle" scope="col">'+"Debit"+'</td>' +
+                '<td align="left" valign="middle" scope="col">'+data+'</td>' +
+                '</tr>';
+        }
+        else
+        {
+            var error_msg = document.getElementById("withdraw-msg");
+            error_msg.innerText = "Don't have sufficient amount in your account!";
+        }
+    }
+}
