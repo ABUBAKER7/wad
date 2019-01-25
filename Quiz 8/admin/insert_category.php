965a5cc40512d070a0090fcf7ba361a2a025033e
ABUBAKER7 <44474481+ABUBAKER7@users.noreply.github.com>
2019-01-25 18:55:17 +0500

    Quiz8 Complete

---
+++ Quiz 8/admin/insert_category.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: l1f16bscs7590
+ * Date: 1/9/2019
+ * Time: 5:34 PM
+ */
---
+++ Quiz 8/searchResultProducts.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: l1f16bscs7590
+ * Date: 1/25/2019
+ * Time: 6:17 PM
+ */
